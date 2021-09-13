4bc9267cea020c895b3d291a64d1db90f7771a2a
TriangleBear <kakanini31@gmail.com>
2021-09-13 17:50:29 +0800

    Update CustomDie.java

--- src/CustomDie.java
+++ src/CustomDie.java
@@ -23,27 +23,35 @@ public class CustomDie{
     }
     public static void addFace(){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Current Die Faces: "+Arrays.toString(nF));
-        int sizenF = nF.length;
-        String newFace = sc.next();
-        String[] newList = new String[sizenF+1];
-        System.arraycopy(nF, 0, newList, 0, nF.length);
-        newList[newList.length-1]=newFace;
-        nF = newList;
-        System.out.println("New Die Face: " + Arrays.toString(nF));
-        System.out.println();
+        if(nF!=null){
+            System.out.println("Current Die Faces: "+Arrays.toString(nF));
+            int sizenF = nF.length;
+            String newFace = sc.next();
+            String[] newList = new String[sizenF+1];
+            System.arraycopy(nF, 0, newList, 0, nF.length);
+            newList[newList.length-1]=newFace;
+            nF = newList;
+            System.out.println("New Die Face: " + Arrays.toString(nF));
+            System.out.println();
+        }else{
+            System.out.println("The Die has Empty Faces");
+        }
         Die_Runner.main(null);
     }
     public static void removeFace(){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Current Die Faces");
-        System.out.println(Arrays.toString(nF));
-        ArrayList<String> str = new ArrayList<>(List.of(nF));
-        System.out.print("What face do you want to remove(n): ");
-        int pos=sc.nextInt();
-        str.remove(pos-1);
-        System.out.println("New Faces: \n"+str);
-        System.out.println();
+        if(nF!=null){
+            System.out.println("Current Die Faces");
+            System.out.println(Arrays.toString(nF));
+            ArrayList<String> str = new ArrayList<>(List.of(nF));
+            System.out.print("What face do you want to remove(n): ");
+            int pos=sc.nextInt();
+            str.remove(pos-1);
+            System.out.println("New Faces: \n"+str);
+            System.out.println();
+        }else{
+            System.out.println("The Die has Empty Faces");
+        }
         Die_Runner.main(null);
     }
 }
