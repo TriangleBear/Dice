a78c43dbf26a2fb05a118a8b6b1b23cddca66fbb
TriangleBear <kakanini31@gmail.com>
2021-09-13 17:44:29 +0800

    Added Equal_CD and Custom_CD for Probability

--- src/UnfairDie.java
+++ src/UnfairDie.java
@@ -1,3 +1,28 @@
-public class UnfairDie extends CustomDie{
-
+import java.util.Random;
+import java.util.Scanner;
+public class UnfairDie extends CustomDie {
+    public static int rn;
+    public static void Equal_CD(){
+        if(CustomDie.nF != null){
+            int rnd = new Random().nextInt(CustomDie.nF.length);
+            System.out.println(nF[rnd]);
+        }else{
+            System.out.println("The Die has Empty Faces");
+            Die_Runner.main(null);
+        }
+        Die_Runner.main(null);
+    }
+    public static void Custom_CD(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Probability for all faces: ");
+        rn = sc.nextInt();
+        if((CustomDie.nF != null)&&(rn<=0)){
+            int rnd = new Random(rn).nextInt(CustomDie.nF.length);
+            System.out.println(nF[rnd]);
+        }else{
+            System.out.println("The Die has Empty Faces");
+            Die_Runner.main(null);
+        }
+        Die_Runner.main(null);
+    }
 }
